527eb9084a0edb78790fc0319cd186b835101036
cganzon <crganzon@gmail.com>
2021-06-17 01:52:20 -0700

    added a close icon when the mobile navbar is toggled on

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 let navBurger = document.querySelector('.burger');
 let popUpNav = document.querySelector('.nav-links');
 let popUpAccent = document.querySelector('.popup-accent');
+let burger = document.querySelector('.burger');
+let closeIcon = document.querySelector('.close-icon');
 
 navBurger.addEventListener('click', () => {
-    popUpNav.classList.toggle('nav-active');
-    popUpAccent.classList.toggle('nav-active');
+    popUpNav.classList.toggle('show-navbar');
+    popUpAccent.classList.toggle('show-navbar');
+    navBurger.classList.remove('show-navbar');
+    navBurger.classList.add('hide-navbar');
+    closeIcon.classList.toggle('show-navbar');
+})
+
+closeIcon.addEventListener('click', () => {
+    popUpNav.classList.toggle('show-navbar');
+    popUpAccent.classList.toggle('show-navbar');
+    navBurger.classList.toggle('show-navbar')
+    closeIcon.classList.toggle('show-navbar')
 })
